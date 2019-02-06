95d7c5d33f31816c8faf77e9c8572102ff371969
Joe Corwin <joe.corwin@gmail.com>
2019-02-06 23:13:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,31 +14,7 @@ function bumpCounter() {
     getBumps
   }
 }
-//
-// * Create a function, `createAnimal()`, that takes one parameter, `animalType`.
-// * Create an inner function in this `createAnimal()` function that also has one parameter, `deadlyDevice`. This function
-// then returns an object that forms our diabolical creation. This object has two properties, `animalType` and `deadlyDevice`
-// that are the given arguments to both functions.
-// * An example of the output: `{ animalType: 'Monkey', deadlyDevice: 'Lightsaber' }`
-// * Create a shark creator function by calling `createAnimal()` with the `'Shark'` string. This should be assigned to a
-// variable called `sharkCreator`.
-// * Add a variable called `sharkWithFrickinLaserbeam` that is a combination of a `'Shark'` and `'Laserbeam'`.
-// * Add a variable called `sharkWithFrickinCannon` that is a combination of a `'Shark'` and `'Cannon'`.
 
-// function createAnimal(animalType) {
-//
-//    function dangerousAnimals(deadlyDevice) {
-//     return {
-//       animalType: animalType,
-//       deadlyDevice: deadlyDevice
-//     }
-// }
-// return dangerousAnimals
-// }
-//
-//  var sharkCreator = createAnimal("Shark")
-// var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
-// var sharkWithFrickinCannon = sharkCreator("Cannon")
 
 function createAnimal(animalType) {
   function dangerousAnimals(deadlyDevice) {
@@ -49,13 +25,6 @@ function createAnimal(animalType) {
   return dangerousAnimals
 }
 
-var sharkCreator = createAnimal('Shark');
-
-var sharkWithFrickinLaserbeam = {
-
-
-}
-
-
-
-var sharkWithFrickinCannon
+var sharkCreator = createAnimal("Shark");
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam");
+var sharkWithFrickinCannon = sharkCreator("Cannon")
